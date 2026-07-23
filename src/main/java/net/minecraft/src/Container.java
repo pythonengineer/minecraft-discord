b8e4a70e7f869547758a63eb12638b6e34b11323
pythonengineer <16440418+pythonengineer@users.noreply.github.com>
2026-07-22 19:01:41 -0700

    Update Container.java

--- src/main/java/net/minecraft/src/Container.java
+++ src/main/java/net/minecraft/src/Container.java
@@ -204,7 +204,6 @@ public abstract class Container {
                                 slot7.putStack(itemstack10);
                                 var3.inventory.setItemStack(itemstack9);
                             }
-                        } else if (itemstack9.getItem() == itemstack10.getItem() && itemstack10.getMaxStackSize() > 1) {
                         } else if (itemstack9.getItem() == itemstack10.getItem() && itemstack10.getMaxStackSize() > 1
                                 && (!itemstack9.getHasSubtypes()
                                         || itemstack9.getItemDamage() == itemstack10.getItemDamage())) {
